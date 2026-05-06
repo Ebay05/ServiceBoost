ae1f84a9ab3b5f7a5ff224afc135f80ce4abf4d2
Ebay05 <ebay11990011@gmail.com>
2026-05-06 09:01:14 +0200

    Updated calendar styles and added a list of events

--- frontend/src/app/features/calendar/calendar.ts
+++ frontend/src/app/features/calendar/calendar.ts
@@ -14,11 +14,26 @@ import {
   CalendarModule,
 } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { Button } from 'primeng/button';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { FormsModule } from '@angular/forms';
+
+interface Event {
+  date: string;
+  description: string;
+  hour?: string;
+  client?: string;
+  vehicle?: string;
+  type: string;
+  age?: number;
+}
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
   imports: [
+    SelectButtonModule,
+    FormsModule,
     CalendarModule,
     CalendarPreviousViewDirective,
     CalendarTodayDirective,
@@ -26,6 +41,7 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
     CalendarMonthViewComponent,
     CalendarWeekViewComponent,
     CalendarDayViewComponent,
+    Button,
     CalendarDatePipe,
   ],
   providers: [
@@ -44,11 +60,56 @@ export class Calendar {
   view: CalendarView = CalendarView.Month;
   viewDate = new Date();
 
-  events: CalendarEvent[] = [
+  myEvents: Event[] = [
     {
-      start: new Date(),
-      title: 'An event',
+      date: '2026-05-01',
+      description: 'Wizyta klienta',
+      hour: '10:30',
+      client: 'Beata Skalska',
+      vehicle: 'Mazda RX60',
+      type: 'work',
     },
+    {
+      date: '2026-05-05',
+      client: 'Michał Wawel',
+      description: 'Serwis auta',
+      vehicle: 'Audi A8',
+      hour: '14:00',
+      type: 'work',
+    },
+    {
+      date: '2026-05-05',
+      client: ' Natalia Czerwińska',
+      vehicle: 'Tiroc 200',
+      description: 'Serwis auta',
+      hour: '12:00',
+      type: 'work',
+    },
+    {
+      date: '2026-05-06',
+      client: 'Adrian Górski',
+      description: 'Serwis auta',
+      vehicle: 'Subaru TR',
+      hour: '16:00',
+      type: 'work',
+    },
+    {
+      date: '2026-05-06',
+      description: 'Urodziny Damiana',
+      age: 43,
+      type: 'birthday',
+    },
+  ];
+
+  events: CalendarEvent[] = this.myEvents.map((e) => ({
+    start: new Date(`${e.date}T${e.hour ?? '00:00'}`),
+    title: e.description,
+  }));
+
+  viewOptions = [
+    { label: 'Miesiąc', value: CalendarView.Month },
+    { label: 'Tydzień', value: CalendarView.Week },
+    { label: 'Dzień', value: CalendarView.Day },
   ];
 
   setView(view: CalendarView) {
