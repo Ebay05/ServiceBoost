a328eebe124048e5dbe36bcb92becbc18f2dfc6a
Ebay05 <ebay11990011@gmail.com>
2026-05-11 07:40:55 +0200

    Updated vercel.json

--- backend/ServiceBoost.API/obj/Debug/net10.0/ServiceBoost.API.AssemblyInfo.cs
+++ backend/ServiceBoost.API/obj/Debug/net10.0/ServiceBoost.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceBoost.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c2141aec6b822ad90ea332728508b742db2fac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d1c9eacd2da071ff2913083d44364802e51850c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceBoost.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceBoost.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
